86a85fedb1340f4b634baf0fdaa772904e58be99
Osman <42710856+osmanakol@users.noreply.github.com>
2020-05-06 13:42:59 +0300

    constructor

--- headers/smallClassroom.hpp
+++ headers/smallClassroom.hpp
@@ -2,6 +2,7 @@
 #define smallClassRoom
 #include "classroom.hpp"
 class SmallClassRoom:public ClassRoom{
-
+    public:
+        SmallClassRoom(int numberOfClass):ClassRoom(numberOfClass){};
 };
 #endif
