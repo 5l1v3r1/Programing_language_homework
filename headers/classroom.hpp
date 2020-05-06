54caa3d09ac942696beb6b9d4eac866b1cbcbca5
Osman <42710856+osmanakol@users.noreply.github.com>
2020-05-06 13:34:31 +0300

    write a method of classroom

--- headers/classroom.hpp
+++ headers/classroom.hpp
@@ -2,6 +2,16 @@
 #define classroom
 
 class ClassRoom{
+    private:
+        int numberOfClass;
+    public:
+        int getNumberOfClass(){
+            return this -> numberOfClass;
+        };
+        void setNumberOfClass(int classNumber){
+            this.numberOfClass = classNumber;
+        }
+
 
 };
 #endif
