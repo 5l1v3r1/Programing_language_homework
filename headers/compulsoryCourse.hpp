cb2175a6ebd04aee93960d5c36a5ad5e4e0b6754
Osman <42710856+osmanakol@users.noreply.github.com>
2020-05-06 13:34:48 +0300

    make a inheritance and write constructor

--- headers/compulsoryCourse.hpp
+++ headers/compulsoryCourse.hpp
@@ -1,7 +1,17 @@
-#ifndef
-#define compulsoryCourse
+#include <string>
 #include "course.hpp"
-class CompulsoryCourse:public Course{
+using namespace std;
+class CompulsoryCourse:public Course
+{
+    private:
+        string type;
+    public:
+        CompulsoryCourse(string courseCode,string courseName,string instructorName,int grade,int credit,string type="C"):Course(courseCode,courseName,instructorName,grade,credit){
+            this-> type = type;
+        };
+        string getType(){
+            return this -> type;
+        }
 
 };
-#endif
+
