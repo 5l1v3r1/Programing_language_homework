909f42f38bf6a22f14ef70434c5eb1b243f83111
Osman <42710856+osmanakol@users.noreply.github.com>
2020-05-06 13:35:35 +0300

    tes program

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
+#include "headers/compulsoryCourse.hpp"
 using namespace std;
 
 
 int main(){
 
+    vector<CompulsoryCourse> compulsoryCourse;
+    CompulsoryCourse cc("asdasd","asdsad","adsads",5,10);
+    compulsoryCourse.push_back(cc);
 
+    //vector<CompulsoryCourse>::iterator it;
 
-
-
+    for(int i = 0; i<compulsoryCourse.size();i++){
+        cout<<compulsoryCourse[i].getType() << endl;
+    }
 
 
     return 0;
