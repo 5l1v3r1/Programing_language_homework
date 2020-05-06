792c94c75d65c0a55b2074c3fec3acd9e737b114
Osman <42710856+osmanakol@users.noreply.github.com>
2020-05-06 13:35:08 +0300

    write a property and constructor

--- headers/course.hpp
+++ headers/course.hpp
@@ -1,7 +1,19 @@
-#ifndef
-#define course
-
+#include <string>
+using namespace std;
 class Course{
+    private:
 
+    public:
+        string courseCode,
+        courseName,
+        instructorName;
+        int grade,credit;
+        Course(string courseCode,string courseName,string instructorName,int grade,int credit){
+            this->courseCode = courseCode;
+            this->courseName = courseName;
+            this->instructorName = instructorName;
+            this->credit = credit;
+            this->grade = grade;
+    }
 };
-#endif
+
