a532bd6e6bc710e6889824fad89594b192b7a064
Osman <42710856+osmanakol@users.noreply.github.com>
2020-05-06 13:42:51 +0300

    constructur and add property of type and get method that property

--- headers/electiveCourse.hpp
+++ headers/electiveCourse.hpp
@@ -1,7 +1,13 @@
-#ifndef
-#define electiveCourse
+#include <string>
 #include "course.hpp"
 class ElectiveCourse:public Course{
-
+    private:
+        string type;
+    public:
+        ElectiveCourse(string courseCode,string courseName,string instructorName,int grade,int credit,string type="E"):Course(courseCode,courseName,instructorName,grade,credit){
+            this-> type = type;
+        };
+        string getType(){
+            return this -> type;
+        }
 };
-#endif
